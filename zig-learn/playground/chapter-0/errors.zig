f02b88c17b8a7831ba57ebaa5880a9a9978ffca8
LitoMore <litomore@gmail.com>
2022-07-17 02:59:19 +0800

    Learning `Errors`

---
+++ zig-learn/playground/chapter-0/errors.zig
@@ -0,0 +1,14 @@
+const expect = @import("std").testing.expect;
+
+const FileOpenError = error{
+    AccessDenied,
+    OutOfMemory,
+    FileNotFound,
+};
+
+const AllocationError = error{OutOfMemory};
+
+test "coerce error from a subset to a superset" {
+    const err: FileOpenError = AllocationError.OutOfMemory;
+    try expect(err == FileOpenError.OutOfMemory);
+}
---
+++ zig-learn/playground/chapter-0/functions.zig
@@ -0,0 +1,28 @@
+const expect = @import("std").testing.expect;
+
+// Simple function
+fn addFive(x: u32) u32 {
+    return x + 5;
+}
+
+test "function" {
+    const y = addFive(0);
+    try expect(@TypeOf(y) == u32);
+    try expect(y == 5);
+}
+
+// Recursion
+fn fibonacci(n: u16) u16 {
+    if (n == 0 or n == 1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+test "function recursion" {
+    const x = fibonacci(10);
+    try expect(x == 55);
+}
+
+// Underscore
+fn _(x: u16) u16 {
+    _ = x;
+}
