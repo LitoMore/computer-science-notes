473336592a76affb969f4cfac50f2c63d1295b57
LitoMore <litomore@gmail.com>
2022-08-04 23:56:40 +0800

    Learning ex15

---
+++ learn-ruby-the-hard-way/playground/ex15.rb
@@ -0,0 +1,11 @@
+filename = ARGV.first
+txt = open(filename)
+puts "Here's your file #{filename}:"
+print txt.read
+txt.close()
+
+print "Type the filename again: "
+file_again = $stdin.gets.chomp
+txt_again = open(file_again)
+print txt_again.read
+txt_again.close()
